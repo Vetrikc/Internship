78f8acd8cfe0ae232acb86519a6a06410009c496
Vetrikc <vllad2676@gmail.com>
2025-11-07 16:27:22 +0300

    feet: db schema User/PaymentCard initialize

---
+++ src/main/java/com/innowise/internship/entitiy/User.java
@@ -0,0 +1,50 @@
+package com.innowise.internship.entitiy;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "idx_users_email", columnList = "email"),
+        @Index(name = "idx_users_active", columnList = "active")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private Boolean active;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<PaymentCard> paymentCards;
+
+}
